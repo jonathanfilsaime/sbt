65b97f80633372f9eb0fc25d8419c47ef634d755
jonathanfilsaime <jonathanfilsaime@gmail.com>
2020-10-30 15:24:17 -0500

    pioeline is working

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,11 +1,11 @@
-import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{HashingTF, StopWordsRemover, Tokenizer}
+import org.apache.spark.ml.feature.{HashingTF, StopWordsRemover, StringIndexer, Tokenizer}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{Row, SparkSession}
-
+import org.apache.spark.sql.SparkSession
 
 object Hello {
+
 
   def main(args: Array[String]) = {
 
@@ -23,6 +23,15 @@ object Hello {
 
     val cleanData = dataCleanUp(tweetCSV)
 
+//    val tempData = spark.createDataFrame(cleanData).toDF("id", "text", "label")
+
+//    val indexer = new StringIndexer()
+//      .setInputCol("label")
+//      .setOutputCol("numericLabel")
+//      .fit(tempData)
+//    val cleanDataIndexed = indexer.transform(tempData)
+    //    val cleanDataIndexedRDD = cleanData.rdd.map(x => (x.get(1), x.get(2), x.get(3), x.get(4))
+
 //    cleanData.foreach(x => println("-------> " + x))
 
     val num = cleanData.count()
@@ -31,9 +40,17 @@ object Hello {
     val trainingPercent = (num * .8).toInt
     println(trainingPercent)
 
+
+
     //training data selection 80%
-    val twitterDataFrame = spark.createDataFrame(cleanData.take(trainingPercent)).toDF("id", "text", "label")
+    val twitterDataFrame = spark.createDataFrame(cleanData.take(trainingPercent)).toDF("id", "text", "stringLabel")
     println("number of training data rows: " + twitterDataFrame.count())
+
+    val indexer = new StringIndexer()
+      .setInputCol("stringLabel")
+      .setOutputCol("label")
+      .fit(twitterDataFrame)
+    val twitterDataFrameIndexed = indexer.transform(twitterDataFrame)
 
     //Configure an ML pipeline, which consists of four stages: tokenizer, StopWordsRemover, hashingTF, and lr.
     val tokenizer = new Tokenizer()
@@ -53,17 +70,30 @@ object Hello {
       .setStages(Array(tokenizer, remover, hashingTF, lr))
 
     //fit the pipeline to training documents.
-    val model = pipeline.fit(twitterDataFrame)
+    val model = pipeline.fit(twitterDataFrameIndexed)
 
     //test data selection 20%
     val trainCleanData = cleanData.take(trainingPercent)
     val testData = cleanData.filter( x => !trainCleanData.contains(x))
     val cleanDataWithoutLabel = testData.map(x => (x._1, x._2))
 
-    val twitterTestDataFrame = spark.createDataFrame(cleanDataWithoutLabel).toDF("id", "text")
+
+
+    val twitterTestDataFrame = spark.createDataFrame(testData).toDF("id", "text", "stringLabel")
     println("number of test data rows: " + twitterTestDataFrame.count())
 
-    val prediction = model.transform(twitterTestDataFrame)
+    //for verifaction and accuracy
+    val indexerTest = new StringIndexer()
+      .setInputCol("stringLabel")
+      .setOutputCol("label")
+      .fit(twitterDataFrame)
+    val twitterDataFrameTestIndexed = indexer.transform(twitterTestDataFrame)
+
+
+    val twitterTestWithoutLabelDataFrame = spark.createDataFrame(cleanDataWithoutLabel).toDF("id", "text")
+    println("number of test data rows: " + twitterTestWithoutLabelDataFrame.count())
+
+    val prediction = model.transform(twitterTestWithoutLabelDataFrame)
       .select("id", "text", "probability", "prediction")
       .collect()
 
@@ -80,7 +110,6 @@ object Hello {
       val column = x.split(",")
       if (column(0).matches("^(0|[1-9][0-9]*)$")
         && !column(1).isEmpty
-        && !(assign(column(1)).equals(9))
         && !column(0).equals("569851578276048896")
         && !column(0).equals("569473998519578624")
         && !column(0).equals("567800574051151872")
@@ -90,32 +119,15 @@ object Hello {
         && !column(0).equals("568757671819661314")
         && !column(0).equals("568637541513089024"))
       {
-        (column(0), column(10), assign(column(1)))
+        (column(0), column(10), column(1))
       }
       else
       {
-        (null, null, 7)
+        (null, null, null)
       }
     }
 
-    val cleanData = data.filter(x => x._1 != null || x._2 != null || x._3 != 7)
+    val cleanData = data.filter(x => x._1 != null || x._2 != null || x._3 != null)
     cleanData
-  }
-
-  def assign(value : Any): Int   = {
-
-      if(value != null && "negative".equalsIgnoreCase(value.toString))
-      {
-        return  0
-      }
-      else if (value != null && "positive".equalsIgnoreCase(value.toString))
-      {
-        return 1
-      }
-      else
-      {
-        return 9
-      }
-
   }
 }
