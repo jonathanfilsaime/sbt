6b88994578571d4b2919c0a37eb6e7722c97e330
jonathanfilsaime <jonathanfilsaime@gmail.com>
2020-10-29 21:47:32 -0500

    pipeline is working

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.ml.{Pipeline, PipelineModel}
 import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature.{HashingTF, Tokenizer}
+import org.apache.spark.ml.feature.{HashingTF, StopWordsRemover, Tokenizer}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{Row, SparkSession}
 
 
@@ -20,9 +21,62 @@ object Hello {
       .textFile("./Tweets.csv")
       .rdd
 
+    val cleanData = dataCleanUp(tweetCSV)
+
+//    cleanData.foreach(x => println("-------> " + x))
+
+    val num = cleanData.count()
+    println("number of total rows: " + num)
+
+    val trainingPercent = (num * .8).toInt
+    println(trainingPercent)
+
+    //training data selection 80%
+    val twitterDataFrame = spark.createDataFrame(cleanData.take(trainingPercent)).toDF("id", "text", "label")
+    println("number of training data rows: " + twitterDataFrame.count())
+
+    //Configure an ML pipeline, which consists of four stages: tokenizer, StopWordsRemover, hashingTF, and lr.
+    val tokenizer = new Tokenizer()
+      .setInputCol("text")
+      .setOutputCol("words")
+    val remover = new StopWordsRemover()
+      .setInputCol(tokenizer.getOutputCol)
+      .setOutputCol("filtered")
+    val hashingTF = new HashingTF()
+      .setNumFeatures(1000)
+      .setInputCol(remover.getOutputCol)
+      .setOutputCol("features")
+    val lr = new LogisticRegression()
+      .setMaxIter(10)
+      .setRegParam(0.001)
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, remover, hashingTF, lr))
+
+    //fit the pipeline to training documents.
+    val model = pipeline.fit(twitterDataFrame)
+
+    //test data selection 20%
+    val trainCleanData = cleanData.take(trainingPercent)
+    val testData = cleanData.filter( x => !trainCleanData.contains(x))
+    val cleanDataWithoutLabel = testData.map(x => (x._1, x._2))
+
+    val twitterTestDataFrame = spark.createDataFrame(cleanDataWithoutLabel).toDF("id", "text")
+    println("number of test data rows: " + twitterTestDataFrame.count())
+
+    val prediction = model.transform(twitterTestDataFrame)
+      .select("id", "text", "probability", "prediction")
+      .collect()
+
+    //print prediction
+    prediction.foreach(println)
+  }
+
+  private def dataCleanUp(tweetCSV: RDD[String]) = {
+
     val header = tweetCSV.first()
-    val tweetCSVWithoutheader = tweetCSV.filter( x => x !=header)
-    val data = tweetCSVWithoutheader.map{ x =>
+    val tweetCSVWithoutheader = tweetCSV.filter(x => x != header)
+
+    val data = tweetCSVWithoutheader.map { x =>
       val column = x.split(",")
       if (column(0).matches("^(0|[1-9][0-9]*)$")
         && !column(1).isEmpty
@@ -37,72 +91,24 @@ object Hello {
         && !column(0).equals("568637541513089024"))
       {
         (column(0), column(10), assign(column(1)))
-      } else {
+      }
+      else
+      {
         (null, null, 7)
       }
     }
 
     val cleanData = data.filter(x => x._1 != null || x._2 != null || x._3 != 7)
-
-    cleanData.foreach(x => println("-------> " + x))
-
-    val num = cleanData.count()
-    println(num)
-
-    val trainingPercent = (num * .8).toInt
-    println(trainingPercent)
-
-    val twitterDataFrame = spark.createDataFrame(cleanData.take(trainingPercent)).toDF("id", "text", "label")
-    println(twitterDataFrame.count())
-
-//     Configure an ML pipeline, which consists of three stages: tokenizer, hashingTF, and lr.
-    val tokenizer = new Tokenizer()
-      .setInputCol("text")
-      .setOutputCol("words")
-    val hashingTF = new HashingTF()
-      .setNumFeatures(1000)
-      .setInputCol(tokenizer.getOutputCol)
-      .setOutputCol("features")
-    val lr = new LogisticRegression()
-      .setMaxIter(10)
-      .setRegParam(0.001)
-    val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, hashingTF, lr))
-
-    // Fit the pipeline to training documents.
-    val model = pipeline.fit(twitterDataFrame)
-
-    // Now we can optionally save the fitted pipeline to disk
-    model.write.overwrite().save("./tmp/spark-logistic-regression-model")
-
-    // We can also save this unfit pipeline to disk
-    pipeline.write.overwrite().save("./tmp/unfit-lr-model")
-
-    // And load it back in during production
-//    val sameModel = PipelineModel.load("/tmp/spark-logistic-regression-model")
-
-    val cleanDataWithoutLabel = cleanData.map(x => (x._1, x._2))
-
-    val twitterTestDataFrame = spark.createDataFrame(cleanDataWithoutLabel).toDF("id", "text")
-    println(twitterTestDataFrame.count())
-
-    val prediction = model.transform(twitterTestDataFrame)
-      .select("id", "text", "probability", "prediction")
-      .collect()
-
-    prediction.foreach(println)
-
+    cleanData
   }
 
   def assign(value : Any): Int   = {
 
-    if(value != null)
-    {
-      if("negative".equalsIgnoreCase(value.toString))
+      if(value != null && "negative".equalsIgnoreCase(value.toString))
       {
         return  0
       }
-      else if ("positive".equalsIgnoreCase(value.toString))
+      else if (value != null && "positive".equalsIgnoreCase(value.toString))
       {
         return 1
       }
@@ -111,10 +117,5 @@ object Hello {
         return 9
       }
 
-    }
-    else
-    {
-      return 0
-    }
   }
 }
